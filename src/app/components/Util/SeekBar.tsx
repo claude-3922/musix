f384625791fa0506a4e0ec42ace2842c6f083346
claude-3922 <aminabdulrehman532@gmail.com>
2024-09-02 07:27:48 +0500

    need to show this to sumone

--- src/app/components/Util/SeekBar.tsx
+++ src/app/components/Util/SeekBar.tsx
@@ -1,10 +1,83 @@
-import React, { HTMLAttributes } from "react";
+import React, {
+  HTMLAttributes,
+  useEffect,
+  useState,
+  MouseEvent,
+  use,
+  useRef,
+  useMemo,
+  CSSProperties,
+} from "react";
 
-interface SeekBarProps extends HTMLAttributes<HTMLSpanElement> {
+interface SeekBarProps {
   width: string;
   height: string;
+  containerStyles: CSSProperties;
+
+  progressPercentage: number;
+  progressStyles: CSSProperties;
+
+  onSeek?: (newValue: number) => void;
 }
 
-export default function SeekBar() {
-  return <span></span>;
+export default function SeekBar({
+  width,
+  height,
+  containerStyles,
+  progressPercentage,
+  progressStyles,
+  onSeek,
+}: SeekBarProps) {
+  const seekbarContainer = useRef<HTMLSpanElement | null>(null);
+
+  const [current, setCurrent] = useState(0);
+  useEffect(() => setCurrent(progressPercentage), [progressPercentage]);
+
+  const seekHandler = (event: MouseEvent<HTMLSpanElement>) => {
+    if (seekbarContainer.current) {
+      const newPercentage =
+        ((event.clientX - seekbarContainer.current.offsetLeft) /
+          seekbarContainer.current.offsetWidth) *
+        100;
+      setCurrent(newPercentage);
+
+      if (onSeek) {
+        onSeek(newPercentage);
+      }
+    }
+  };
+
+  return (
+    <>
+      <span
+        className="hover:cursor-pointer"
+        onClick={(e) => seekHandler(e)}
+        style={{ paddingTop: height, paddingBottom: height }}
+      >
+        <span
+          className="flex flex-col items-start justify-center"
+          ref={seekbarContainer}
+          style={{
+            position: "relative",
+            width: width,
+            height: height,
+            overflowX: "hidden",
+            overflowY: "hidden",
+            ...containerStyles,
+          }}
+        >
+          <span
+            style={{
+              position: "absolute",
+              zIndex: 1,
+              height: height,
+              width: `${current}%`,
+              transition: "width 0.02s linear",
+              ...progressStyles,
+            }}
+          ></span>
+        </span>
+      </span>
+    </>
+  );
 }
